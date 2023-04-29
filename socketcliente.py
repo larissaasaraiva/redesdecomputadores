2aec7389cf520dc99c635f0430b611ee5093e993
Larissa Saraiva <lariissasaraiva@gmail.com>
2023-04-29 17:04:05 -0300

    Conexão Socket

--- socketcliente.py
+++ socketcliente.py
@@ -6,3 +6,7 @@ Port = 10000
 
 # Criar um objeto socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectar o socket à porta onde o servidor está escutando
+server_address = (Host, Port)
+sock.connect(server_address)
