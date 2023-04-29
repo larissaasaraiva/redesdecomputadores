8b6f912b2c172a42ffc608fa86a7317b6a0728e2
Eric Lucena <eric.lucena12365@gmail.com>
2023-04-29 18:32:46 -0300

    Definindo e criando o objeto socket servidor

---
+++ SocketServidor.py
@@ -0,0 +1,13 @@
+import socket
+
+#nome da maquina e numero da porta
+
+HOST = 'localhost'
+PORT = 10000
+
+#CRIANDO INVOCANDO O MÉTODO SOCKET
+
+#AF INET(IPV4), STREAM POIS É TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen
