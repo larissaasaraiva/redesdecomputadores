29aad906bb0bcdcefc5c15be6c345f34dee0f976
Larissa Saraiva <lariissasaraiva@gmail.com>
2023-04-29 17:21:24 -0300

    Função para iniciar o servidor

--- socketcliente.py
+++ socketcliente.py
@@ -4,28 +4,33 @@ import socket
 Host = 'localhost'
 Port = 10000
 
-# Criar um objeto socket TCP/IP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Função para iniciar o servidor
+def Client(Host, Port):
+    # Criar um objeto socket TCP/IP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conectar o socket à porta onde o servidor está escutando
-server_address = (Host, Port)
-sock.connect(server_address)
+    # Conectar o socket à porta onde o servidor está escutando
+    server_address = (Host, Port)
+    sock.connect(server_address)
+    print('Conectado ao servidor {} na porta {}'.format(*server_address))
 
-# Gerando um numero inteiro aleatorio com ate 30 digitos
-num = random.randint(1, 999999999999999999999999999999)  
+    # Gerando um numero inteiro aleatorio com ate 30 digitos
+    num = random.randint(1, 999999999999999999999999999999)  
 
-# Enviando o numero gerado para o servidor
-sock.sendall(str(num).encode())
+    # Enviando o numero gerado para o servidor
+    sock.sendall(str(num).encode())
+    print('Numero enviado para o servidor: {}'.format(num))
 
-# Recebendo a resposta do servidor
-data = sock.recv(1024)
-Resp = data.decode() + "FIM"
+    # Recebendo a resposta do servidor
+    data = sock.recv(1024)
+    Resp = data.decode() + "FIM"
 
-# Imprimindo a resposta do servidor
-print(Resp)
+    # Imprimindo a resposta do servidor
+    print('Resposta do servidor: {}'.format(Resp))
 
-# Devolvendo a resposta do servidor
-sock.sendall(Resp.encode())
+    # Devolvendo a resposta do servidor
+    sock.sendall(Resp.encode())    
 
-# Fechando a conexão
-sock.close()
+    # Fechando a conexão
+    sock.close()
+    print('Conexão encerrada com o servidor {} na porta {}'.format(*server_address))
